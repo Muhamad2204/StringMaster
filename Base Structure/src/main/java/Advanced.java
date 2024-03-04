2449c717ea44d51828473ee35a5edc527a15205f
Sponge Bob <Muhamad22204>
2024-03-04 20:19:54 +0330

    files added

--- Base Structure/src/main/java/Advanced.java
+++ Base Structure/src/main/java/Advanced.java
@@ -10,7 +10,17 @@ public class Advanced {
 
      */
     public String wordCensor(String sentence, String word, String newWord){
-        return null;
+        StringBuilder result = new StringBuilder();
+        String[] words = sentence.split(" ");
+        for(String w : words){
+            if(w.equals(word)){
+                result.append(newWord).append(" ");
+            }
+            else{
+                result.append(w).append(" ");
+            }
+        }
+        return result.toString().trim();
     }
 
     /**
@@ -20,7 +30,15 @@ public class Advanced {
      * @return fullName is a normal full name that just the first letter of firstName & lastName is Capitalized (example : Harry Potter)
      */
     public String normalizingName(String firstName, String lastName){
-        return null;
+        if(!firstName.equals(" ")){
+            String normalFirstName = firstName.substring(0, 1 ).toUpperCase() + firstName.substring(1).toLowerCase();
+            if(!lastName.equals(" ")){
+                String normalLastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();
+                return normalFirstName + " " + normalLastName;
+            }
+            return normalFirstName;
+        }
+        return "";
     }
 
     /**
@@ -29,7 +47,15 @@ public class Advanced {
      * @return if word contains Consecutive repeated letters, one of the repeated letters should be omitted
      */
     public String doubleChar(String word) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        char prevChar = '\0';
+        for(char currentChar : word.toCharArray()){
+            if(currentChar != prevChar) {
+                result.append(currentChar);
+                prevChar = currentChar;
+            }
+        }
+        return result.toString();
     }
 }
 
--- Base Structure/src/main/java/Warmup.java
+++ Base Structure/src/main/java/Warmup.java
@@ -6,7 +6,13 @@ public class Warmup {
      * @return is the number th word of the sentence
      */
     public String wordFinder(String sentence, int number) {
-        return null;
+        String word[] = sentence.split(" ");
+        if(number > 0 && number <= word.length){
+            return word[number - 1];
+        }
+        else{
+            return "Number = " + number + " is out Of Bound";
+        }
     }
 
     /**
@@ -16,7 +22,17 @@ public class Warmup {
      * @return if searchForEven is true ? return the number of even numbers : return the number of odd numbers
      */
     public int oddEvenCounter(String number, boolean searchForEven) {
-        return -1;
+        char[] numArray = number.toCharArray();
+        int count = 0;
+        for(char num : numArray){
+            int  n = Character.getNumericValue(num);
+            if(searchForEven && n%2==0){
+                count++;
+            } else if (!searchForEven && n%2!=0) {
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
@@ -25,6 +41,16 @@ public class Warmup {
      * @return The word that is first in alphabet column
      */
     public String firstWord(String wordA, String wordB) {
-        return null;
+        int minlength = Math.min(wordA.length(), wordB.length());
+        for(int i = 0; i < minlength; i++){
+            char char1 = wordA.charAt(i);
+            char char2 = wordB.charAt(i);
+            if(char1 > char2){
+                return wordB;
+            } else if (char1 < char2) {
+                return wordA;
+            }
+        }
+        return minlength == wordA.length() ? wordA : wordB;
     }
 }
